f06a4f479789431cad861f781a597523b132a549
Warren Simpson <warren.simpson34@gmail.com>
2020-07-31 09:54:07 -0400

    Added database and models folder with the db connection and models for store and user

---
+++ models/user.js
@@ -0,0 +1,20 @@
+const Sequelize = require("sequelize");
+const db = require("./../database/db");
+
+const User = db.define("users", {
+  user_id: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+  },
+
+  wallet_address: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+
+}, {
+  timestamps: false
+});
+
+module.exports = User;
