795a54e4fac40489d142c2c6ee7b670f07471888
Warren Simpson <warren.simpson34@gmail.com>
2020-09-15 20:23:14 -0400

    Updated the storeHub abi, change the storeHub smart contract test address, and finished the MetaDataUpdated function so metaData can now be updated in the database.

--- event_handling/MetaDataUpdated.js
+++ event_handling/MetaDataUpdated.js
@@ -1,8 +1,28 @@
-const db = require('../database/db');
+const Store = require('../models/store');
 
-function MetaDataUpdated(metaData) {
-    console.log(metaData);
-    //[0x76E976D4c734B68E6816309D884Dd7E42A794d55, 0x76E976D4c734B68E6816309D884Dd7E42A794d55, 0x76E976D4c734B68E6816309D884Dd7E42A794d55, 0x76E976D4c734B68E6816309D884Dd7E42A794d55, 0x76E976D4c734B68E6816309D884Dd7E42A794d55]
+async function MetaDataUpdated(store_id, metaData) {
+    try {
+
+        //Update the metaData in the database.
+        await Store.update(
+            {
+                country: metaData[0],
+                city: metaData[1],
+                street: metaData[2],
+                store_type: metaData[3],
+                website: metaData[4]
+            },
+            {
+                where: {
+                    store_id: store_id
+                }
+            }
+        )
+
+    }
+    catch (err) {
+        console.log(err);
+    }
 }
 
 module.exports = MetaDataUpdated;
